c3d825dfad60976eb73b9258234b1811d9028d78
Feng DING <ding.feng@etu.unice.fr>
2015-05-22 01:03:58 +0200

    ss

--- projetARServer/src/webservice/WebServiceImpl.java
+++ projetARServer/src/webservice/WebServiceImpl.java
@@ -1,21 +1,15 @@
 package webservice;
 
 import java.rmi.RemoteException;
-<<<<<<< HEAD
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 
-public class WebServiceImpl extends UnicastRemoteObject implements WebService{
+public class WebServiceImpl extends UnicastRemoteObject implements WebService {
 
 	protected WebServiceImpl() throws RemoteException {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-=======
-import java.util.ArrayList;
-
-public class WebServiceImpl implements WebService {
->>>>>>> f96bf74f169c032fba28c3f3d2ea83db96729af7
 
 	@Override
 	public boolean register() throws RemoteException {
