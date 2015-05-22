426e08b0a67ad78efd97b1b0f4e89c6adda135fa
Tianhao Shi <shi.tianhao@etu.unice.fr>
2015-05-23 00:10:25 +0200

    merge

--- projetARClient/src/stub/Service.java
+++ projetARClient/src/stub/Service.java
@@ -1,8 +1,9 @@
 package stub;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface Service {
+public interface Service extends Remote{
 	public boolean addFollowing(String topicName) throws RemoteException;
 	
 //	public boolean deleteFollowing(String topicName) throws RemoteException;
